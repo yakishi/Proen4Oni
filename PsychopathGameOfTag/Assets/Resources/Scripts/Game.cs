2053eeb945d5db2e229061adc4d0872f084ecc45
Terada <shin,yakishi222@gmail.com>
2017-09-05 22:26:16 +0900

    Game.csの追加

---
+++ PsychopathGameOfTag/Assets/Resources/Scripts/Game.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+
+    /// <summary>
+    /// タイマー
+    /// </summary>
+    static public Timer timer;
+    public Timer Timer { get { return timer; } }
+
+    /// <summary>
+    /// ターン
+    /// </summary>
+    public enum Turn
+    {
+        red,
+        blue
+    }
+
+    static private Turn turn_;
+    public Turn turn {
+        set
+        {
+            turn_ = value;
+        }
+
+        get
+        {
+            return turn_;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        timer = new Timer(60.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (timer.isRunning()) {
+            timer.Update();
+        }
+	}
+}
