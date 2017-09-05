efb1067e7294fd53cfbb709c99032b84ec849723
Terada <shin,yakishi222@gmail.com>
2017-09-05 22:26:44 +0900

    TimerクラスとGUITimerの追加

---
+++ PsychopathGameOfTag/Assets/Resources/Scripts/GUI/TimeGUI.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeGUI : MonoBehaviour {
+
+    /// <summary>
+    /// ゲーム
+    /// </summary>
+    [SerializeField]
+    Game game;
+
+    /// <summary>
+    /// タイマー
+    /// </summary>
+    Timer timer;
+
+    /// <summary>
+    /// テキスト
+    /// </summary>
+    [SerializeField]
+    Text timeText;
+
+	// Use this for initialization
+	void Start () {
+        timer = game.Timer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer = game.Timer;
+
+        Debug.Log(timer.RemainTime.ToString());
+
+        if(timer.RemainTime > 0.0f) {
+            timeText.text = timer.RemainTime.ToString();
+
+            if (game.turn == Game.Turn.red) {
+                timeText.color = Color.red;
+            }
+            else {
+                timeText.color = Color.blue;
+            }
+        }
+        else {
+            timer.Reset();
+
+            if (game.turn == Game.Turn.red) {
+                game.turn = Game.Turn.blue;
+            }
+            else {
+                game.turn = Game.Turn.red;
+            }
+        }
+    }
+}
